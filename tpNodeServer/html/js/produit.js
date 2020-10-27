d1673a5881b7a2e62e6d82dba11b835f25eb492a
didier <didier@d-defrance.fr>
2020-10-27 11:58:59 +0100

    s

---
+++ tpNodeServer/html/js/produit.js
@@ -0,0 +1,22 @@
+function onSelection(evt){
+   //console.log( evt.target.nodeName + ' - ' +  evt.target.id);
+   var td = evt.target;
+   var trParent  = td.parentNode;
+   console.log("id="+trParent.id);
+}
+
+window.onload = function () {
+    console.log("code declenche des le chargement de la page dans le navigateur");
+
+    var prodA = { code : 1 , nom : "cahier" , prix : 1.2 };
+
+    var bodyTableau = document.getElementById("bodyTableau");
+    var newRow = bodyTableau.insertRow(-1);
+    newRow.setAttribute("id","p_"+ prodA.code);
+    var cellCode = newRow.insertCell(0);
+    cellCode.innerText=prodA.code;
+    newRow.insertCell(1).innerText=prodA.nom;
+    newRow.insertCell(2).innerText=prodA.prix;
+
+    newRow.addEventListener("click",onSelection);
+}
