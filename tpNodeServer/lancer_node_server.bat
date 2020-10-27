d0ce26ac0f6b414013e37be30c09568a5a646316
didier <didier@d-defrance.fr>
2020-10-27 09:07:23 +0100

    avec devise-api-routes.js

---
+++ tpNodeServer/lancer_node_server.bat
@@ -0,0 +1,2 @@
+REM nodedemon server.js
+node server.js
--- tpNodeServer/server.js
+++ tpNodeServer/server.js
@@ -1,19 +1,24 @@
 var express = require('express');
-//var deviseApiRoutes = require('./devise-api-routes');
+var deviseApiRoutes = require('./devise-api-routes');
 var produitApiRoutes = require('./produit-api-routes');
 var bodyParser = require('body-parser');
+
 var app = express();
 //support parsing of JSON post data
 var jsonParser = bodyParser.json() ;
 app.use(jsonParser);
+
 //les routes en /html/... seront gérées par express
 //par de simples renvois des fichiers statiques du répertoire "./html"
 app.use('/html', express.static(__dirname+"/html"));
+
 app.get('/', function(req , res ) {
 res.redirect('/html/index.html');
 });
-//*app.use(deviseApiRoutes.apiRouter);* //delegate REST API routes to apiRouter(s)
+
+app.use(deviseApiRoutes.apiRouter); //delegate REST API routes to apiRouter(s)
 app.use(produitApiRoutes.apiRouter);
+
 app.listen(8282 , function () {
-console.log("http://localhost:8282");
+  console.log("http://localhost:8282");
 });
