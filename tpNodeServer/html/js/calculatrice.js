9277bcc4a86f35f093ca809b33ec0443c416d579
didier <didier@d-defrance.fr>
2020-10-27 10:08:22 +0100

    calculatrice javascript

---
+++ tpNodeServer/html/js/calculatrice.js
@@ -0,0 +1,10 @@
+function additionner(){
+    var txtA = document.getElementById('txtA');//zone input
+    var a = Number(txtA.value);
+    var txtB = document.getElementById('txtB');//zone input
+    var b = Number(txtB.value);
+    var res = a +b;
+    var spanRes = document.getElementById('spanRes');//zone span
+    spanRes.innerHTML="<b>"+res+"</b>";
+    //spanRes.innerText=res;
+}
