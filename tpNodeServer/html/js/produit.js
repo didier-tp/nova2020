01e119db6d591f9b816b6f6c9a47bbc996fdf2c8
didier <didier@d-defrance.fr>
2020-10-27 14:19:55 +0100

    avec appels ajax en GET et en PUT

--- tpNodeServer/html/js/produit.js
+++ tpNodeServer/html/js/produit.js
@@ -3,10 +3,18 @@ function onSelection(evt){
    var td = evt.target;
    var trParent  = td.parentNode;
    console.log("id="+trParent.id);
+   var codeProd = trParent.id.substring(2); //apres p_
+   makeAjaxGetRequest("../produit-api/public/produit/"+codeProd ,
+                       function (data){
+                          var prod = JSON.parse(data);
+                          document.getElementById("spanCode").innerText=prod.code;
+                          document.getElementById("txtNom").value=prod.nom;
+                          document.getElementById("txtPrix").value=prod.prix;
+                       });
 }
 
 function recupererListeProduit(data){
-   console.log("data="+data); //data est au format JSON
+  // console.log("data="+data); //data est au format JSON
    var tabProduits = JSON.parse(data);
    for(let prod of tabProduits){
       ajouterLigneDansTableau(prod);
@@ -26,11 +34,37 @@ function ajouterLigneDansTableau(prod){
     newRow.addEventListener("click",onSelection);
 }
 
+function callbackApresMaj(data){
+   var spanMessage  = document.getElementById("spanMessage");
+   spanMessage.innerHTML="mise à jour ok : " + data ;
+   //rafraichir les valeurs du tableau
+   //ici via nouvel appel ajax (sans optimisation):
+   var bodyTableau = document.getElementById("bodyTableau");
+   bodyTableau.innerHTML="";
+   makeAjaxGetRequest("../produit-api/public/produit" , recupererListeProduit);
+}
+
+function errCallback(data){
+   var spanMessage  = document.getElementById("spanMessage");
+   spanMessage.innerHTML="error : " + data ;
+}
+
+function updateProduit(){
+   var prod = { code : undefined , nom : undefined , prix : undefined};
+   prod.code = Number(document.getElementById("spanCode").innerText);
+   prod.nom = document.getElementById("txtNom").value;
+   prod.prix = Number(document.getElementById("txtPrix").value);
+   var url = "../produit-api/private/role-admin/produit";
+   var jsonData = JSON.stringify(prod);
+   makeAjaxPutRequest(url,jsonData,callbackApresMaj,errCallback);
+}
+
 window.onload = function () {
-    console.log("code declenche des le chargement de la page dans le navigateur");
+    //console.log("code declenche des le chargement de la page dans le navigateur");
 
     makeAjaxGetRequest("../produit-api/public/produit" , recupererListeProduit);
 
-
+    var btnUpdate =  document.getElementById("btnUpdate");
+    btnUpdate.addEventListener("click",updateProduit);
    
 }
