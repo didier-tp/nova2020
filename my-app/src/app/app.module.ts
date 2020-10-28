aafc37162df06df9f4f16a5643ac69c2ea16672f
didier <didier@d-defrance.fr>
2020-10-28 11:47:41 +0100

    avec sous composants

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BasicComponent } from './basic/basic.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasicComponent,
+    CalculatriceComponent,
+    TvaComponent
   ],
   imports: [
     BrowserModule,
